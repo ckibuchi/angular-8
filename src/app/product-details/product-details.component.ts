1be7431a65d90f9d2aa35be1d9cbb741454e12d9
ckibuchi <chriskibuchics@gmail.com>
2019-12-06 12:00:53 +0300

    Added httpclient but with an error

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 
 import {products} from '../products';
+import {CartService} from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -11,7 +12,10 @@ import {products} from '../products';
 export class ProductDetailsComponent implements OnInit {
 
 product;
-  constructor(private route: ActivatedRoute,) { 
+  constructor(
+    private route: ActivatedRoute,
+    private cartService: CartService,
+    ) { 
   }
 
   ngOnInit() {
@@ -20,6 +24,12 @@ product;
       this.product= products[+params.get('productId')];
     }
     )
+  }
+
+  addToCart(product)
+  {
+    window.alert("Product has been added successfully" );
+    this.cartService.addToCart(product);
   }
 
 }
